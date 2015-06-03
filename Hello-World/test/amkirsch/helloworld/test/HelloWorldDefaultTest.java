f00446ff70162ab348ef66250bb8fa9b5bb178dd
noober01 <noobertest@gmx.com>
2015-06-03 12:33:46 -0400

    Added HelloWorld Java Application to begin development.

---
+++ Hello-World/test/amkirsch/helloworld/test/HelloWorldDefaultTest.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package amkirsch.helloworld.test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import amkirsch.helloworld.HelloWorld;
+/**
+ *
+ * @author amkirsch
+ */
+public class HelloWorldDefaultTest {
+    
+    public HelloWorldDefaultTest() {
+        
+    }
+    
+    @Test
+    public void SmokeScreen(){
+        HelloWorld hw = new HelloWorld("Hallo World!");
+        assertEquals(hw.getMessage(), "Hallo World!");
+    }
+}
